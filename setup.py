4812b8ce8f614cfd7ea7e8983813d512c727e26d
Florian Hönicke <hoenicke.florian@gmail.com>
2021-06-15 02:17:03 +0200

    refactor: remove comments

--- setup.py
+++ setup.py
@@ -16,7 +16,3 @@ setuptools.setup(
     install_requires=open("requirements.txt").readlines(),
     python_requires=">=3.7",
 )
-
-
-# from jinahub.encoder.image.clip import CLIPImageEncoder
-# from jinahub.encoder.text.clip import CLIPTextEncoder
