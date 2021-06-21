e67d9028ea02bd8af57b5572ca38a1048b4891fa
bwanglzu <bo.wang@jina.ai>
2021-06-21 22:44:06 +0200

    fix: replace traversal path to paths

--- tests/unit/test_clip.py
+++ tests/unit/test_clip.py
@@ -3,6 +3,6 @@ from jina.executors import BaseExecutor
 def test_clip():
     ex = BaseExecutor.load_config('../../config.yml')
     assert ex.default_batch_size == 32
-    assert ex.default_traversal_path == 'r'
+    assert ex.default_traversal_paths == 'r'
     assert ex.device == 'cpu'
-    assert ex.is_updated == False
+    assert ex.is_updated is False
