92c70047c3fcc0d2f61057798e9654a60ca94507
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2021-07-22 19:15:45 +0800

    fix: redundant flow integration test to unit test

--- tests/integration/test_clip.py
+++ tests/integration/test_clip.py
@@ -1,111 +1,14 @@
-import operator
+import torch
 import os
-from glob import glob
-
 import clip
 import numpy as np
-import torch
+from glob import glob
 from PIL import Image
-from jina import Flow, Document, DocumentArray, requests
-from jina.executors import BaseExecutor
+
+from jina import Flow, Document
 from jinahub.encoder.clip_image import CLIPImageEncoder
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-def test_clip_any_image_shape():
-    f = Flow().add(uses=CLIPImageEncoder)
-    with f:
-        results = f.post(
-            on='/test', inputs=[Document(blob=np.ones((224, 224, 3), dtype=np.uint8))], return_results=True)
-        assert len(results[0].docs.get_attributes('embedding')) == 1
-
-        results = f.post(
-            on='/test', inputs=[Document(blob=np.ones((100, 100, 3), dtype=np.uint8))], return_results=True)
-        assert len(results[0].docs.get_attributes('embedding')) == 1
-
-
-def test_clip_batch():
-    f = Flow().add(uses={
-        'jtype': CLIPImageEncoder.__name__,
-        'with': {
-            'default_batch_size': 10,
-            'model_name': 'ViT-B/32',
-            'device': 'cpu'
-        }
-    })
-    with f:
-        results = f.post(
-            on='/test',
-            inputs=(Document(blob=np.ones((224, 224, 3), dtype=np.uint8)) for _ in range(25)),
-            return_results=True)
-        assert len(results[0].docs.get_attributes('embedding')) == 25
-
-
-def test_traversal_paths():
-
-    blob = np.ones((224, 224, 3), dtype=np.uint8)
-    docs = [Document(
-        id='root1',
-        blob=blob,
-        chunks=[
-            Document(
-                id='chunk11',
-                blob=blob,
-                chunks=[
-                    Document(id='chunk111', blob=blob),
-                    Document(id='chunk112', blob=blob),
-                ]
-            ),
-            Document(id='chunk12', blob=blob),
-            Document(id='chunk13', blob=blob),
-        ]
-    )]
-    f = Flow().add(uses={
-        'jtype': CLIPImageEncoder.__name__,
-        'with': {
-            'default_traversal_paths': ['c'],
-            'model_name': 'ViT-B/32',
-            'device': 'cpu'
-        }
-    })
-    with f:
-        results = f.post(on='/test', inputs=docs, return_results=True)
-        for path, count in [['r', 0], ['c', 3], ['cc', 0]]:
-            assert len(DocumentArray(results[0].docs).traverse_flat([path]).get_attributes('embedding')) == count
-
-        results = f.post(on='/test', inputs=docs, parameters={'traversal_paths': ['cc']}, return_results=True)
-        for path, count in [['r', 0], ['c', 0], ['cc', 2]]:
-            assert len(DocumentArray(results[0].docs).traverse_flat([path]).get_attributes('embedding')) == count
-
-
-def test_custom_processing():
-    f = Flow().add(uses=CLIPImageEncoder)
-    with f:
-        result1 = f.post(on='/test', inputs=[Document(blob=np.ones((224, 224, 3), dtype=np.uint8))],
-                         return_results=True)
-
-    f = Flow().add(uses={
-        'jtype': CLIPImageEncoder.__name__,
-        'with': {
-            'use_default_preprocessing': False,
-        }
-    })
-
-    with f:
-        result2 = f.post(on='/test', inputs=[Document(blob=np.ones((224, 224, 3), dtype=np.float32))],
-                         return_results=True)
-
-    assert result1[0].docs[0].embedding is not None
-    assert result2[0].docs[0].embedding is not None
-    np.testing.assert_array_compare(operator.__ne__, result1[0].docs[0].embedding, result2[0].docs[0].embedding)
-
-
-def test_no_documents():
-    with Flow().add(uses=CLIPImageEncoder) as f:
-        results = f.post(on='/test', inputs=[], return_results=True)
-        assert results[0].status.code == 0  # SUCCESS
-
 
 def test_clip_data():
     docs = []
--- tests/unit/test_clip.py
+++ tests/unit/test_clip.py
@@ -1,4 +1,90 @@
+import operator
+import copy
+
+import numpy as np
+
+from jina import Flow, Document, DocumentArray
 from jina.executors import BaseExecutor
+from jinahub.encoder.clip_image import CLIPImageEncoder
+
+
+def test_clip_any_image_shape():
+    encoder = CLIPImageEncoder()
+    docs = DocumentArray([Document(blob=np.ones((224, 224, 3), dtype=np.uint8))])
+
+    encoder.encode(docs=docs, parameters={})
+    assert len(docs.get_attributes('embedding')) == 1
+
+    docs = DocumentArray([Document(blob=np.ones((100, 100, 3), dtype=np.uint8))])
+    encoder.encode(docs=docs, parameters={})
+    assert len(docs.get_attributes('embedding')) == 1
+
+
+def test_clip_batch():
+    encoder = CLIPImageEncoder(default_batch_size=10, model_name='ViT-B/32', device='cpu')
+    docs = DocumentArray([Document(blob=np.ones((224, 224, 3), dtype=np.uint8)) for _ in range(25)])
+    encoder.encode(docs, parameters={})
+    assert len(docs.get_attributes('embedding')) == 25
+
+
+def test_traversal_paths():
+    blob = np.ones((224, 224, 3), dtype=np.uint8)
+    docs = DocumentArray([Document(
+        id='root1',
+        blob=blob,
+        chunks=[
+            Document(
+                id='chunk11',
+                blob=blob,
+                chunks=[
+                    Document(id='chunk111', blob=blob),
+                    Document(id='chunk112', blob=blob),
+                ]
+            ),
+            Document(id='chunk12', blob=blob),
+            Document(id='chunk13', blob=blob),
+        ]
+    )])
+
+    encoder = CLIPImageEncoder(default_traversal_paths = ['c'], model_name = 'ViT-B/32', device = 'cpu')
+    encoder.encode(docs, parameters={})
+    for path, count in [['r', 0], ['c', 3], ['cc', 0]]:
+        assert len(docs.traverse_flat([path]).get_attributes('embedding')) == count
+
+    #results = f.post(on='/test', inputs=docs, parameters={'traversal_paths': ['cc']}, return_results=True)
+    encoder = CLIPImageEncoder(default_traversal_paths=['cc'])
+    encoder.encode(docs, parameters={})
+    for path, count in [['r', 0], ['c', 0], ['cc', 2]]:
+        assert len(docs.traverse_flat([path]).get_attributes('embedding')) == count
+
+
+def test_custom_processing():
+    f = Flow().add(uses=CLIPImageEncoder)
+    with f:
+        result1 = f.post(on='/test', inputs=[Document(blob=np.ones((224, 224, 3), dtype=np.uint8))],
+                         return_results=True)
+
+    f = Flow().add(uses={
+        'jtype': CLIPImageEncoder.__name__,
+        'with': {
+            'use_default_preprocessing': False,
+        }
+    })
+
+    with f:
+        result2 = f.post(on='/test', inputs=[Document(blob=np.ones((224, 224, 3), dtype=np.float32))],
+                         return_results=True)
+
+    assert result1[0].docs[0].embedding is not None
+    assert result2[0].docs[0].embedding is not None
+    np.testing.assert_array_compare(operator.__ne__, result1[0].docs[0].embedding, result2[0].docs[0].embedding)
+
+
+def test_no_documents():
+    encoder = CLIPImageEncoder()
+    docs = DocumentArray()
+    encoder.encode(docs=docs)
+    assert len(docs) == 0  # SUCCESS
 
 
 def test_clip():
